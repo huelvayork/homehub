35757826b6425af42c077e57694eb0991ceb4f40
David <davidp@huelvayork.com>
2013-09-11 12:39:50 +0200

    CSS corregido

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
+<link href="css/boton.css" rel="stylesheet"/>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>HomeHub</title>
-<link href="./css/boton.css" rel="stylesheet">
 </head>
 <body>
 <a href="#" class="boton">Activar 1 cosa</a>
