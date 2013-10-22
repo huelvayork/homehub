26cdc41c6178f68396ae2f14f7d8b09a1f130f35
davidp <davidp@huelvayork.com>
2013-10-22 21:30:38 +0200

    Empty dialog box contents before showing. (previous content was showed)

--- js/homehub.js
+++ js/homehub.js
@@ -2,6 +2,7 @@ function hhServiceSetup() {
 	$(".hhService").click(function(caller) {
 		serviceName = caller.target.name;
 		$("#hhDialogTitle").text("Control Servicios - " + serviceName);
+		$("#hhDialogBody").text("");
 		$("#hhDialog").modal();
 		$("#hhDialogBody").load("lib/ServiceHandler-helper.php?action=show&service=" + serviceName);
 	});	
